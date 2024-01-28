c247fda3fbcd98ece8a4004b957cc01eaeae7439
qwertikkkb <147443772+qwertikkkb@users.noreply.github.com>
2024-01-28 19:05:14 +0200

    First Commit

---
+++ Slot Machine/Slot Machine/Slot_MachineApp.swift
@@ -0,0 +1,17 @@
+//
+//  Slot_MachineApp.swift
+//  Slot Machine
+//
+//  Created by Nicolai Bodean on 28.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct Slot_MachineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
