4dcd31b2298474a957aa69d0bb203ec89eef2cf8
qwertikkkb <147443772+qwertikkkb@users.noreply.github.com>
2024-03-11 18:50:14 +0200

    Animation

--- Slot Machine/Slot Machine/Views/ContentView.swift
+++ Slot Machine/Slot Machine/Views/ContentView.swift
@@ -20,6 +20,8 @@ struct ContentView: View {
     @State private var isActiveBet10 = true
     @State private var isActiveBet20 = false
     @State private var showingModel = false
+    @State private var animatingSymbol = false
+    @State private var animatingModel = false
     
      //Mark - Functions
     
@@ -144,6 +146,12 @@ struct ContentView: View {
                             Image(symbols[reels[0]])
                                 .resizable()
                                 .modifier(ImageModifier())
+                                .opacity(animatingSymbol ? 1 : 0)
+                                .offset(y: animatingSymbol ? 0 : -50)
+                                .animation(.easeOut(duration: Double.random(in: 0.5...0.7)))
+                                .onAppear(perform: {
+                                    self.animatingSymbol.toggle()
+                                })
                              
                         }
                         
@@ -154,6 +162,12 @@ struct ContentView: View {
                                 Image(symbols[reels[1]])
                                     .resizable()
                                     .modifier(ImageModifier())
+                                    .opacity(animatingSymbol ? 1 : 0)
+                                    .offset(y: animatingSymbol ? 0 : -50)
+                                    .animation(.easeOut(duration: Double.random(in: 0.7...0.9)))
+                                    .onAppear(perform: {
+                                        self.animatingSymbol.toggle()
+                                    })
                            
                             }
                             
@@ -164,17 +178,36 @@ struct ContentView: View {
                                 Image(symbols[reels[2]])
                                     .resizable()
                                     .modifier(ImageModifier())
+                                    .opacity(animatingSymbol ? 1 : 0)
+                                    .offset(y: animatingSymbol ? 0 : -50)
+                                    .animation(.easeOut(duration: Double.random(in: 0.9...1.1)))
+                                    .onAppear(perform: {
+                                        self.animatingSymbol.toggle()
+                                    })
                                   
                             }
                         }
                         .frame(maxWidth: 500)
                          //Mark - Spin Button
                         Button( action: {
-                          //Spin the reels
+                            // 1. Set the default state: No animation
+                            withAnimation {
+                                self.animatingSymbol = false
+                            }
+                            
+                          // 2. Spin the reels with changing the symbols
                             self.spinReels()
                             
-                            //Check winning
+                            // 3. Trigger the animation after changing the symbols
+                            withAnimation {
+                                self.animatingSymbol = true
+                            }
+                            
+                            // 4. Check winning
                             self.checkWining()
+                            
+                            // 5. Game is over
+                            self.isGameOver()
                 
                         }) {
                             Image("gfx-spin")
@@ -198,6 +231,7 @@ struct ContentView: View {
                         HStack(alignment: .center, spacing: 10) {
                             Image("gfx-casino-chips")
                                 .resizable()
+                                .offset(x: isActiveBet10 ? 0 : -20)
                                 .opacity(isActiveBet10 ? 1 : 0)
                                 .modifier(CasinoChipsModifier())
                             
@@ -232,6 +266,7 @@ struct ContentView: View {
                            )
                            Image("gfx-casino-chips")
                                .resizable()
+                               .offset(x: isActiveBet20 ? 0 : 20)
                                .opacity(isActiveBet20 ? 1 : 0)
                                .modifier(CasinoChipsModifier())
                        }
@@ -259,18 +294,84 @@ struct ContentView: View {
                         self.showingInfoView = true
                     }) {
                         Image(systemName: "info.circle")
-                        
-                          
-                            
-                            
+            
                     }
                     .modifier(ButtonModifier()),
                     alignment: .topTrailing
-                    
                 )
                 
                 .padding()
                 .frame(maxWidth: 720)
+                .blur(radius: $showingModel.wrappedValue ? 5 : 0, opaque: false)
+                
+                 //Mark - POPUP
+                if $showingModel.wrappedValue {
+                    ZStack {
+                        Color("ColorTransparentBlack").ignoresSafeArea(edges: .all)
+                        
+                        //Model
+                        VStack(spacing: 0) {
+                            //Title
+                            Text("GAME OVER")
+                                .font(.system(.title, design: .rounded))
+                                .fontWeight(.heavy)
+                                .padding()
+                                .frame(minWidth: 0, maxWidth: .infinity)
+                                .background(Color("ColorPink"))
+                                .foregroundStyle(Color.white)
+                            
+                            Spacer()
+                            
+                            //Message
+                            VStack(alignment: .center, spacing: 16) {
+                                Image("gfx-seven-reel")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(maxHeight: 72)
+                                
+                                Text("Bad Luck! You lost all of the coins. \nLet's play again!")
+                                    .font(.system(.body, design: .rounded))
+                                    .lineLimit(2)
+                                    .multilineTextAlignment(.center)
+                                    .foregroundStyle(Color.gray)
+                                    .layoutPriority(1)
+                                
+                                Button(action: {
+                                    self.showingModel = false
+                                    self.animatingModel = false
+                                    self.activateBet10()
+                                    self.coins = 100
+                                }) {
+                                    Text("New Game".uppercased())
+                                        .font(.system(.body, design: .rounded))
+                                        .fontWeight(.semibold)
+                                        .accentColor(Color("ColorPink"))
+                                        .padding(.horizontal, 12)
+                                        .padding(.vertical, 8)
+                                        .frame(minWidth: 128)
+                                        .background(
+                                        Capsule()
+                                            .strokeBorder(lineWidth: 1.75)
+                                            .foregroundStyle(Color("ColorPink"))
+                                        )
+                                }
+                            }
+                            
+                            Spacer()
+                            
+                        }
+                        .frame(minWidth: 280, idealWidth: 280, maxWidth: 320, minHeight: 260, idealHeight: 280, maxHeight: 320, alignment: .center)
+                        .background(Color.white)
+                        .cornerRadius(20)
+                        .shadow(color: Color("ColorTransparentBlack"), radius: 6, x: 0, y: 8)
+                        .opacity($animatingModel.wrappedValue ? 1 : 0)
+                        .offset(y: $animatingModel.wrappedValue ? 0 : -100)
+                        .animation(Animation.spring(response: 0.6, dampingFraction: 1.0, blendDuration: 1.0))
+                        .onAppear(perform: {
+                            self.animatingModel = true
+                        })
+                    }
+                }
             }//ZStack
             .sheet(isPresented: $showingInfoView) {
                 InfoView()
